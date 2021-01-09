34c16d4f472f118451692da8be293ed25e9e8206
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-01-09 21:10:04 +0400

    Delete firma.20.11.py

--- mehsul_firma/firma.20.11.py
+++
@@ -1,7 +0,0 @@
-class Firma:
-    def __init__(self,firma_adi,hansi_mehsullar,firma_kategoriyasi):
-
-        self.ad=firma_adi
-        self.mehsullar=hansi_mehsullar
-        self.kategoriya=firma_kategoriyasi
-
